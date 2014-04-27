91e6477abb33ca888571af88a20c9a18a6704743
unlcky <0@eightyfiveconsulting.com>
2014-04-26 19:56:01 -0700

    Functioning local game  prototype
    @ryanhedges
    Create compareHistory function for comparing current move with move
    history
    Modify onDrop function to reject move if compareHistory function is null
    Modify onDrop to highlight previous moves with greySquare function
    Create knight.js, modify game_over function to only check for checkmate
    or stalemate
    Mod

--- assets/js/kbr.js
+++ assets/js/kbr.js
@@ -1,9 +1,9 @@
 var board,
-    game = new Chess(),
+    game = new Chess('n7/8/8/8/8/8/8/7N w - - 0 1'),
     statusEl = $('#status'),
     fenEl = $('#fen'),
     pgnEl = $('#pgn');
-
+    var startPOS = ["Nh1","Na8"]
 var onDragStart = function(source, piece, position, orientation) {
   if (game.game_over() === true ||
       (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
@@ -12,22 +12,62 @@ var onDragStart = function(source, piece, position, orientation) {
   }
 };
 
+function compareHistory(target) {
+  // loop through history array
+  // compare target to move history
+  // return null if match
+  var his = game.history()
+  his.pop()
+  his.unshift(startPOS[0], startPOS[1])
+  for(var i=0; i < (his.length-1); i++) {
+    console.log("For")
+    if(target === his[i].substr(1)) {
+      console.log("hey we are in compareHistory")
+      return null
+    }
+  }
+};
+
+var greySquare = function(square) {
+  var squareEl = $('#board .square-' + square);
+  
+  var background = '#a9a9a9';
+  if (squareEl.hasClass('black-3c85d') === true) {
+    background = '#696969';
+  }
+
+  squareEl.css('background', background);
+};
+
 var onDrop = function(source, target) {
   // see if the move is legal
   var move = game.move({
     from: source,
     to: target,
-    promotion: 'q' // NOTE: always promote to a queen for example simplicity
   });
 
+  if (source === target) {
+    return 'snapback';
+  } else {
+    var history = compareHistory(target)
+    if (history === null){
+      game.undo()
+      console.log('Illegal move');
+      return 'snapback';
+    }
+  }
   // illegal move
-  if (move === null) return 'snapback';
-
+  if (move === null) {
+    console.log('Illegal move');
+    return 'snapback';
+  }
+  greySquare(source);
   updateStatus();
 };
 
 var onSnapEnd = function() {
   board.position(game.fen());
+
 };
 
 var updateStatus = function() {
@@ -66,12 +106,15 @@ var updateStatus = function() {
 var cfg = { 
   draggable: true,
   moveSpeed: 'slow',
-  position: {
-    d4: 'bN',
-    e5: 'wN',
-    a1: 'wK',
-    h8: 'bK'
-  },
+  position: 'n7/8/8/8/8/8/8/7N',
+  // position: 'start',
+  // position: {
+  //   d4: 'bN',
+  //   e5: 'wN',
+  //   a1: 'wK',
+  //   h8: 'bK'
+  // },
+
   onDragStart: onDragStart,
   onDrop: onDrop,
   onSnapEnd: onSnapEnd
